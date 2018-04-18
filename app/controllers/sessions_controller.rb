b5a6ee35ecf604024134f0eddd332f59a42c8ced
Chely Ho <adept_inept@yahoo.com>
2018-04-18 00:15:29 +0000

    automatically backed up by learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,7 +5,7 @@ class SessionsController < ApplicationController
   def create
     @user = User.find_by(name: params[:name])
 
-    session[:user_id] = @user.id if   @user.authenticate(params[:password])
+    session[:user_id] = @user.id if @user.authenticate(params[:password])
   end
 
   def destroy
